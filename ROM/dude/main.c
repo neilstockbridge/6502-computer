95dd701c63e8fc2e304610e29fdcb0d8595ba009
Neil Stockbridge <neil@dist.ro>
2015-06-08 19:58:09 +1200

    many changes to the R1 ROM and a ROM uploader

---
+++ ROM/dude/main.c
@@ -0,0 +1,259 @@
+
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+
+#define  SERIAL_DEVICE   "/dev/ttyUSB0"
+
+//#define  BAUDRATE  B115200
+#define  BAUDRATE  B38400
+
+
+#define  ROM_FILE  "/tmp/ROM.prg"
+
+// The mtime of the ROM loaded.  If the file changes then its new mtime won't
+// match this and it must be reloaded.
+time_t  loaded_file_version = 0;
+
+
+int             port;
+struct termios  oldtio;
+
+
+void static  tidy_up()
+{
+  // Restore the configuration of the serial port:
+  tcsetattr( port, TCSANOW, &oldtio );
+}
+
+
+void  init()
+{
+  struct termios  newtio;
+
+  port = open( SERIAL_DEVICE, O_RDWR | O_NOCTTY );
+      if ( port < 0 ) { perror(SERIAL_DEVICE); exit(-1); }
+
+  tcgetattr( port , &oldtio ); // save current port settings
+  atexit( tidy_up );
+
+  bzero( &newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD; // CS8 =>  8 bit characters, CLOCAL => ignore modem control lines
+  newtio.c_iflag = IGNBRK | IGNPAR;
+  newtio.c_oflag = 0;
+
+  // set input mode (non-canonical, no echo,...)
+  newtio.c_lflag = 0;
+
+  newtio.c_cc[VTIME] = 0;   // inter-character timer unused
+  newtio.c_cc[VMIN]  = 1;   // blocking read until 5 chars received
+
+  tcflush( port, TCIFLUSH );
+  tcsetattr( port, TCSANOW, &newtio );
+}
+
+
+void static  send( uint8_t data )
+{
+  ssize_t  bytes_written = write( port, &data, 1 );
+      if ( bytes_written < 1 ) { printf("write\n"); perror( SERIAL_DEVICE ); return; }
+}
+
+
+// A .prg file is a file where the first two bytes indicate the 16-bit address
+// at which to load the rest of the file.
+//
+void load_prg_file( char *path_to_file, uint32_t  bytes_in_file )
+{
+  uint16_t  base_address; // The address at which the data should be loaded
+  ssize_t   bytes_read; // The number of bytes read by read()
+  uint16_t  bytes_to_load = bytes_in_file - 2;
+
+  int  file = open( path_to_file, O_RDONLY );
+      if ( -1 == file ) { perror( path_to_file); return; }
+
+  // Read the first two bytes from the file, which indicate the address at
+  // which to load the rest
+  bytes_read = read( file, &base_address, 2 );
+      if ( bytes_read < 2 ) {
+        fprintf( stderr, "%s: too short\n", path_to_file );
+        return;
+      }
+      if ( 65536 < base_address + bytes_to_load )
+      {
+        fprintf( stderr, "%s: would overflow 64K\n", path_to_file );
+        return;
+      }
+
+  // Begin the UPLOAD request
+  send('u');
+  // Send the load_address
+  send( base_address & 0xff );
+  send( base_address >> 8 );
+  // Send the number of bytes in the ROM
+  send( bytes_to_load & 0xff );
+  send( bytes_to_load >> 8 );
+
+  while ( 0 < bytes_to_load )
+  {
+    uint8_t  data;
+
+    if ( (bytes_read & 0xff) == 0 )
+    {
+      fprintf( stdout, ".");
+      fflush( stdout );
+    }
+
+    bytes_read = read( file, &data, 1 );
+      if ( bytes_read != 1 ) {
+        fprintf( stderr, "%s: corrupt\n", path_to_file );
+        return;
+      }
+
+    send( data );
+
+    bytes_to_load -= 1;
+  }
+
+  close( file );
+  printf("done.\n");
+}
+
+
+void static  loop()
+{
+  struct stat  st;
+  int          outcome;
+
+  // Determine the size and mtime of ROM_FILE
+  outcome = stat( ROM_FILE, &st );
+      if ( outcome != 0 ) { perror( ROM_FILE ); return; }
+
+  // If the file has changed ( or not yet been loaded)
+  if ( st.st_mtime != loaded_file_version )
+  {
+    printf("File change detected.  Uploading..\n");
+    sleep(1);
+    load_prg_file( ROM_FILE, st.st_size );
+    printf("Resetting\n");
+    send('r');
+    loaded_file_version = st.st_mtime;
+  }
+
+  sleep(1);
+}
+
+
+uint8_t  read8()
+{
+  ssize_t   bytes_read;
+  uint8_t  value;
+
+  bytes_read = read( port, &value, 1 );
+      if ( bytes_read != 1 ) { printf("read8 %lu\n", bytes_read); perror( SERIAL_DEVICE); exit(1); }
+
+  return value;
+}
+
+
+uint16_t  read16()
+{
+  return read8() | read8() << 8;
+}
+
+
+void  download( char  device, char *path_to_file )
+{
+  ssize_t  bytes_read, bytes_written;
+
+  int  file = open( path_to_file, O_CREAT|O_WRONLY|O_TRUNC, S_IRUSR|S_IWUSR );
+      if ( -1 == file ) { perror( path_to_file); exit(1); }
+
+  send( device );
+  uint16_t  base_address = read16();
+  uint16_t  bytes = read16();
+  printf("%u bytes at $%04x\n", bytes, base_address );
+  bytes_written = write( file, &base_address, 2 );
+    if ( bytes_written != 2 ) { perror( path_to_file); exit(1); }
+  while ( 0 < bytes )
+  {
+    uint8_t  data;
+    bytes_read = read( port, &data, 1 );
+      if ( bytes_read != 1 ) { printf("read\n"); perror( SERIAL_DEVICE); exit(1); }
+    bytes_written = write( file, &data, 1 );
+      if ( bytes_written != 1 ) { perror( path_to_file); exit(1); }
+    bytes -= 1;
+  }
+
+  close( file );
+}
+
+
+void  usage()
+{
+  fprintf( stderr, "Use: dude upload|(download rom|ram output-file)\n");
+  exit(1);
+}
+
+
+bool streq( char *test, char *s )
+{
+  return 0 == strcmp( test, s );
+}
+
+
+int main( int argc, char *argv[])
+{
+  enum
+  {
+    UPLOAD_AND_WATCH,
+    DOWNLOAD,
+  }
+  mode;
+  char  device;
+  char  *output_file;
+
+  if ( argc < 2 )
+    usage();
+
+  else {
+    init();
+
+    if ( streq("upload",argv[1]) )
+      mode = UPLOAD_AND_WATCH;
+
+    else if ( streq("download", argv[1]) )
+    {
+      mode = DOWNLOAD;
+      if ( streq("rom",argv[2]) )
+        device = 'd';
+      else if ( streq("ram",argv[2]) )
+        device = 'D';
+      else
+        usage();
+      output_file = argv[3];
+    }
+    else
+      usage();
+  }
+
+  if ( mode == UPLOAD_AND_WATCH )
+    while ( true)
+    {
+      loop();
+    }
+  else
+    download( device, output_file );
+}
+
---
+++ ROM/instruction-set.rb
@@ -0,0 +1,365 @@
+#!/usr/bin/env ruby
+#
+# Contains and parses a representation of the W65C02 instruction set that is
+# easy to input from the data sheet and outputs it in JSON.
+#
+
+# from data-sheet                   fromDS  bytes cycles  C (for addr. may be used as index to M[] again dep on instr)
+addressing_modes = <<"."
+absolute                            a       2     4m      i2 << 8 | i1
+absolute-indexed-indirect           (a,x)   2     5       M[ ( i2 << 8 | i1 ) + X ]
+absolute-indexed-with-X             a,x     2     4pm     ( i2 << 8 | i1 ) + X
+absolute-indexed-with-Y             a,y     2     4p      ( i2 << 8 | i1 ) + Y
+absolute-indirect                   (a)     2     4m      M[ i2 << 8 | i1 ]
+accumulator                         A       0     2       A
+immediate                           #       1     2       i1
+implied                             i       0     2       -
+program-counter-relative            r       1     2b      i1
+stack                               s       0     3..7
+zero-page                           zp      1     3m      i1
+zero-page-indexed-indirect          (zp,x)  1     6       M[ i1 + X ]
+zero-page-indexed-with-X            zp,x    1     4m      i1 + X
+zero-page-indexed-with-Y            zp,y    1     4       i1 + Y
+zero-page-indirect                  (zp)    1     5       M[ i1 ]
+zero-page-indirect-indexed-with-Y   (zp),y  1     5       M[ i1 ] + Y
+.
+# b: +1 cycle if branch is taken
+# m: +2 cycles for read-modify-write instructions
+# p: +1 cycle if page boundary is crossed when forming address
+
+opcodes = <<".".reject {|line| "----------\n" == line }
+brk a
+ora (zp,x)
+-
+-
+tsb zp
+ora zp
+asl zp
+rmb0 zp
+php s
+ora #
+asl A
+-
+tsb a
+ora a
+asl a
+bbr0 r
+----------
+bpl r
+ora (zp),y
+ora (zp)
+-
+trb zp
+ora zp,x
+asl zp,x
+rmb1 zp
+clc i
+ora a,y
+inc A
+-
+trb a
+ora a,x
+asl a,x
+bbr1 r
+----------
+jsr a
+and (zp,x)
+-
+-
+bit zp
+and zp
+rol zp
+rmb2 zp
+plp s
+and #
+rol A
+-
+bit a
+and a
+rol a
+bbr2 r
+----------
+bmi r
+and (zp),y
+and (zp)
+-
+bit zp,x
+and zp,x
+rol zp,x
+rmb3 zp
+sec i
+and a,y
+dec A
+-
+bit a,x
+and a,x
+rol a,x
+bbr3 r
+----------
+rti s
+eor zp,x
+-
+-
+-
+eor zp
+lsr zp
+rmb4 zp
+pha s
+eor #
+lsr A
+-
+jmp a
+eor a
+lsr a
+bbr4 r
+----------
+bvc r
+eor (zp),y
+eor (zp)
+-
+-
+eor zp,x
+lsr zp,x
+rmb5 zp
+cli i
+eor a,y
+phy s
+-
+-
+eor a,x
+lsr a,x
+bbr5 r
+----------
+rts s
+adc (zp,x)
+-
+-
+stz zp
+adc zp
+ror zp
+rmb6 zp
+pla s
+adc #
+ror A
+-
+jmp (a)
+adc a
+ror a
+bbr6 r
+----------
+bvs r
+adc (zp),y
+adc (zp)
+-
+stz zp,x
+adc zp,x
+adc zp,x
+rmb7 zp
+sei i
+adc a,y
+ply s
+-
+jmp (a,x)
+adc a,x
+ror a,x
+bbr7 r
+----------
+bra r
+sta (zp,x)
+-
+-
+sty zp
+sta zp
+stx zp
+smb0 zp
+dey i
+bit #
+txa i
+-
+sty a
+sta a
+stx a
+bbs0 r
+----------
+bcc r
+sta (zp),y
+sta (zp)
+-
+sty zp,x
+sta zp,x
+stx zp,y
+smb1 zp
+tya i
+sta a,y
+txs i
+-
+stz a
+sta a,x
+stz a,x
+bbs1 r
+----------
+ldy #
+lda (zp,x)
+ldx #
+-
+ldy zp
+lda zp
+ldx zp
+smb2 zp
+tay i
+lda #
+tax i
+-
+ldy a
+lda a
+ldx a
+bbs2 r
+----------
+bcs r
+lda (zp),y
+lda (zp)
+-
+ldy zp,x
+lda zp,x
+ldx zp,y
+smb3 zp
+clv i
+lda a,y
+tsx i
+-
+ldy a,x
+lda a,x
+ldx a,x
+bbs3 r
+----------
+cpy #
+cmp (zp,x)
+-
+-
+cpy zp
+cmp zp
+dec zp
+smb4 zp
+iny i
+cmp #
+dex i
+wai i
+cpy a
+cmp a
+dec a
+bbs4 r
+----------
+bne r
+cmp (zp),y
+cmp (zp)
+-
+-
+cmp zp,x
+dec zp,x
+smb5 zp
+cld i
+cmp a,y
+phx s
+stp i
+-
+cmp a,x
+dec a,x
+bbs5 r
+----------
+cpx #
+sbc (zp,x)
+-
+-
+cpx zp
+sbc zp
+inc zp
+smb6 zp
+inx i
+sbc #
+nop i
+-
+cpx a
+sbc a
+inc a
+bbs6 r
+----------
+beq r
+sbc (zp),y
+sbc (zp)
+-
+-
+sbc zp,x
+inc zp,x
+smb7 zp
+sed i
+sbc a,y
+plx s
+-
+-
+sbc a,x
+inc a,x
+bbs7 r
+.
+
+mode_id_by_label = {}
+
+puts <<"."
+; Table of addressing modes.  Each byte is simply the number of operand bytes
+; for an instruction that uses the mode
+@ADDRESSING_MODES:
+.
+print "  .byt "
+addressing_modes.each_with_index do |line, mode_id| line.chomp!
+  description, label, bytes, cycle, in_C = line.split(/  +/)
+  mode_id_by_label[ label] = mode_id
+  print "#{ bytes }"
+  print ", " unless "(zp),y" == label # "(zp),y" being the last
+end
+puts
+
+
+mnemonics = []           # A list of mnemonics ( strings)
+opcode_mnemonic_ids = [] # Lookup table from OpCode to index in the "mnemonics" table
+opcode_mode_ids = []     # Lookup table from OpCode to index in the "addressing_modes" table
+
+opcodes.each_with_index do |line, opcode| line.chomp!
+  mnemonic, mode_id = line.split
+  # Rename the SMBx, RMBx, BBRx, BBSx mnemonics so they a) fit in to 3
+  # characters, and b) match the Set/Clear convention of the original
+  mnemonic.sub! /smb([0-7])/, 'se\1'
+  mnemonic.sub! /rmb([0-7])/, 'cl\1'
+  mnemonic.sub! /bbs([0-7])/, 'b\1s'
+  mnemonic.sub! /bbr([0-7])/, 'b\1c'
+  mnemonic.sub! "-", "---"
+
+  mnemonics << mnemonic unless mnemonics.include? mnemonic
+  opcode_mnemonic_ids << mnemonics.index( mnemonic)
+  opcode_mode_ids << mode_id_by_label[ mode_id]
+end
+
+puts "; List of mnemonics"
+puts "@MNEMONICS:"
+mnemonics.each_with_index do |mnemonic, mnemonic_id|
+  print "  .byt " if 0 == mnemonic_id & 0x7
+  print "\"#{ mnemonic }\""
+  last = mnemonics.count - 1 == mnemonic_id
+  eol = mnemonic_id + 1 & 0x7 == 0
+  print ! eol && ! last ? ", " : "\n"
+end
+
+puts "; Lookup table that maps OpCode to index in @MNEMONICS"
+puts "@OPCODE_MNEMONIC_IDS:"
+opcode_mnemonic_ids.each_with_index do |mnemonic_id, opcode|
+  print "  .byt " if 0 == opcode & 0xf
+  print "$%02x"% mnemonic_id
+  print opcode + 1 & 0xf != 0 ? "," : "\n"
+end
+
+puts "; Lookup table that maps OpCode to index in @ADDRESSING_MODES"
+puts "@OPCODE_ADDRESSING_MODE_IDS:"
+opcode_mode_ids.each_with_index do |mode_id, opcode|
+  print "  .byt " if 0 == opcode & 0xf
+  print "$%02x"% mode_id
+  print opcode + 1 & 0xf != 0 ? "," : "\n"
+end
+
